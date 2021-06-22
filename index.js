259451b7707da3640b6b9ae6b9a26c7a60feb455
GrietM <marcelagriet@gmail.com>
2021-06-22 17:38:18 -0300

    Agrego funcionalidades de login, token y encriptado de contraseñas

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const bodyParser = require('body-parser')
 
 const Operation = require('./models/operationModel')
 const operationRouter = require('./routes/operationRouter')(Operation) 
+const User = require('./models/userModel.js')
+const userRouter = require('./routes/userRouter.js')(User)
+const jwt = require('express-jwt')
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.use('/api', operationRouter)
+app.use('/api', userRouter)
 
 const connectDB = async () => {
     try {
